f8643eec439cca70cd489a20473e3301b9ab96b1
yohuang <you@example.com>
2019-06-30 16:57:50 -0400

    test pollSCM

--- HelloWorld/src/oata/HelloWorld.java
+++ HelloWorld/src/oata/HelloWorld.java
@@ -2,7 +2,7 @@ package oata;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World Changed");
     }
 }
 
